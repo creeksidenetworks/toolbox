0f8c901e2f2d35f1702ce12096576ee7171560d0
Jackson Tong <creeksidenetworks@gmail.com>
2025-09-11 15:21:15 +0800

    Add Gli-net openwrt support

---
+++ router/gfw/glinet/openwrt/bin/gfw-startup.sh
@@ -0,0 +1,225 @@
+#!/bin/ash
+# (c) 2022-2024 Creekside Networks LLC, Jackson Tong
+# This script will prepare for the gfw jailbreak. Need to run one time after reboot.
+# for Openwrt
+
+export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
+sdw_backup="40.118.161.200"
+# Get the current script's path
+ROOT_PATH="$(dirname $(dirname "$(readlink -f "$0")"))"
+
+CONF_PATH="${ROOT_PATH}/conf"
+SCRIPTS_DIR="${ROOT_PATH}/bin"
+LOG_FILE="/var/log/gfw.log"
+
+# Log function
+log() {
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] $1" | tee -a "$LOG_FILE"
+}
+
+# Log startup
+if [ ! -f "$LOG_FILE" ]; then
+    touch "$LOG_FILE"
+fi
+
+log "Starting gfw-startup script"
+
+
+# Check if coreutils-base64 is installed
+if ! opkg list-installed | grep -q 'coreutils-base64'; then
+    echo "coreutils-base64 is not installed. Installing..."
+    opkg update
+    opkg install coreutils-base64 nano tcpdump
+
+    if [ $? -eq 0 ]; then
+        log "coreutils-base64 installed successfully."
+    else
+        log "Failed to install coreutils-base64."
+        exit 1
+    fi
+else
+    echo "coreutils-base64 is already installed."
+fi
+
+# restart dnsmasq if needed
+pid=$(sudo netstat -tulnp | grep 0.0.0.0:55353 | grep tcp | awk '{print $7}' | cut -d'/' -f1)
+if [ -n "$pid" ]; then
+    kill -9 $pid
+    log "killed dnsmasq process $pid listening on port 55353"
+fi
+
+/usr/sbin/dnsmasq -p 55353 --conf-file="${CONF_PATH}/dnsmasq.conf" 
+
+# Check if dnsmasq service is running
+pid=$(sudo netstat -tulnp | grep 0.0.0.0:55353 | grep tcp | awk '{print $7}' | cut -d'/' -f1)
+if [ -n "$pid" ]; then
+    log "dnsmasq is up with pid=$pid."
+else
+    log "dnsmasq failed to start"
+fi
+
+# Bring up wireguard interfaces
+for config_file in ${ROOT_PATH}/conf/wireguard/*.conf; do
+    interface=$(basename "$config_file" .conf)
+    # Check if the interface already exists and tear it down
+    if ip link show "$interface" > /dev/null 2>&1; then
+        ip link set "$interface" down
+        ip link delete "$interface"
+    fi
+
+    # Read configuration from file
+    address=$(grep 'Address' "$config_file" | awk '{print $3}')
+    private_key=$(grep 'PrivateKey' "$config_file" | awk '{print $3}')
+    public_key=$(grep 'PublicKey' "$config_file" | awk '{print $3}')
+    allowed_ips=$(grep 'AllowedIPs' "$config_file" | awk '{print $3}')
+    endpoint=$(grep 'Endpoint' "$config_file" | awk '{print $3}')
+    persistent_keepalive=$(grep 'PersistentKeepalive' "$config_file" | awk '{print $3}')
+
+
+    # Split endpoint into hostname and port
+    endpoint_host=$(echo "$endpoint" | awk -F':' '{print $1}')
+    endpoint_port=$(echo "$endpoint" | awk -F':' '{print $2}')
+
+    # Resolve endpoint_host to IP if it is a FQDN
+    if ! echo "$endpoint_host" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'; then
+        endpoint_ip=$(nslookup "$endpoint_host" | awk '/^Address 1:/ { print $3; exit }')
+        if [ -z "$endpoint_ip" ]; then
+            log "Failed to resolve $endpoint_host, using backup IP $sdw_backup"
+            endpoint_ip="$sdw_backup"
+        fi
+        endpoint="$endpoint_ip:$endpoint_port"
+    fi
+
+    # Set up WireGuard interface
+    ip link add "$interface" type wireguard
+    ip address add "$address" dev "$interface"
+    
+    temp_private_key_file=$(mktemp)
+    echo "$private_key" > "$temp_private_key_file"
+    wg set "$interface" private-key "$temp_private_key_file"
+    rm "$temp_private_key_file"
+    
+    wg set "$interface" peer "$public_key" allowed-ips "$allowed_ips" endpoint "$endpoint" persistent-keepalive "$persistent_keepalive"
+    ip link set "$interface" up
+
+    # Allow traffic forwarding between br-lan and wgx interfaces
+    iptables -I FORWARD 1 -i br-lan -o "$interface" -j ACCEPT
+    iptables -I FORWARD 1 -i br-guest -o "$interface" -j ACCEPT
+
+    # Enable source NAT for traffic to wgx interfaces
+    iptables -t nat -A POSTROUTING -o "$interface" -j MASQUERADE
+
+    log "$interface configured."
+done
+
+# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Add gooogle DNS routing and gfw routing table
+ip route add 8.8.8.8/32 dev $interface metric 100
+ip route add table 100 default dev $interface metric 100
+
+log "routing table configured."
+
+# create ip ipset LIBERTY_ADDRESS_GRP and add 8.8.8.8/8.8.4.4 to it
+if ! ipset list LIBERTY_ADDRESS_GRP > /dev/null 2>&1; then
+    ipset create LIBERTY_ADDRESS_GRP hash:ip
+    ipset add LIBERTY_ADDRESS_GRP 8.8.8.8
+    ipset add LIBERTY_ADDRESS_GRP 8.8.4.4
+    log "created ipset LIBERTY_ADDRESS_GRP."
+else
+    log "ipset LIBERTY_ADDRESS_GRP already exists."
+fi
+
+# Add policy route for LIBERTY_ADDRESS_GRP
+ip rule add from all fwmark 1 lookup 100
+iptables -t mangle -A PREROUTING -m set --match-set LIBERTY_ADDRESS_GRP dst -j MARK --set-mark 1
+iptables -t mangle -A PREROUTING -s 192.168.9.0/24 -j MARK --set-mark 1
+log "Policy routing for LIBERTY_ADDRESS_GRP configured."
+
+# force local network 192.168.8.0/24 dns requst to smart dns
+iptables -t nat -A PREROUTING -s 192.168.8.0/24 -p udp --dport 53 -j DNAT --to-destination 192.168.8.1:55353
+# force global network 192.168.9.0/24 dns requst to google dns
+iptables -t nat -A PREROUTING -s 192.168.9.0/24 -p udp --dport 53 -j DNAT --to-destination 8.8.8.8:53
+
+log "DNAT for DNS requests configured."
+
+# check if the cron tab file is linked to /etc/crontabs/root
+if [ ! -L /etc/crontabs/root ]; then
+    ln -fs ${ROOT_PATH}/conf/crontabs.root /etc/crontabs/root
+    log "linked crontab file."
+else
+    log "crontab file already linked."
+fi
+
+# update Wifi
+
+# Change all wireless interfaces to 20Mhz channel wide
+uci set wireless.radio0.htmode='HT20'
+uci set wireless.radio1.htmode='HT20'
+uci set wireless.@wifi-device[0].disabled=0
+uci set wireless.@wifi-device[1].disabled=0
+
+# Main WiFi 
+uci set wireless.@wifi-iface[0].ssid="LibertySmart"
+uci set wireless.@wifi-iface[0].encryption="psk2"
+uci set wireless.@wifi-iface[0].key="Good2Great"
+uci set wireless.@wifi-iface[0].disabled=1
+
+uci set wireless.@wifi-iface[1].ssid="LibertySmart"
+uci set wireless.@wifi-iface[1].encryption="psk2"
+uci set wireless.@wifi-iface[1].key="Good2Great"
+uci set wireless.@wifi-iface[1].disabled=0
+
+# Guest WiFi 
+uci set wireless.@wifi-iface[2].ssid="LibertyGlobal"
+uci set wireless.@wifi-iface[2].encryption="psk2"
+uci set wireless.@wifi-iface[2].key="Good2Great"
+uci set wireless.@wifi-iface[2].disabled=1
+
+uci set wireless.@wifi-iface[3].ssid="LibertyGlobal"
+uci set wireless.@wifi-iface[3].encryption="psk2"
+uci set wireless.@wifi-iface[3].key="Good2Great"
+uci set wireless.@wifi-iface[3].disabled=0
+
+# Disable 2.4G radio
+#uci set wireless.radio0.disabled='1'
+
+# Commit the changes
+uci commit wireless
+
+# Reload the Wi-Fi settings
+wifi reload
+
+# Enable guest network
+uci set network.guest.disabled='0'
+uci commit network
+
+# check if this script is linked to /etc/rc.local
+SERVICE_FILE="/etc/init.d/gfw-startup"
+if [ ! -f $SERVICE_FILE ]; then
+    cat << 'EOF' > $SERVICE_FILE
+#!/bin/sh /etc/rc.common
+
+START=99
+
+start_service() {
+    /etc/config/gfw/bin/gfw-startup.sh
+}
+EOF
+    chmod +x $SERVICE_FILE
+    $SERVICE_FILE enable
+    log "Creating startup script service file $SERVICE_FILE"
+else
+    log "service file $SERVICE_FILE already exists"
+fi
+
+# preserve the new file during upgrade
+if cat /etc/sysupgrade.conf | grep -q "$SERVICE_FILE"; then
+    echo "$SERVICE_FILE file already preserved"
+else
+    echo "$SERVICE_FILE" >> "/etc/sysupgrade.conf"
+    echo "Preserve startup script file"
+fi
---
+++ router/gfw/glinet/openwrt/bin/update-gfw-list.sh
@@ -0,0 +1,118 @@
+#!/bin/ash
+
+# Name:        update-gfw-list.sh
+# Desription:  A shell script which convert gfwlist into dnsmasq rules for VyOS/EdgeOS routers.
+#              Reference Cokebar Chi's gfwlist2dnsmasq.sh (https://github.com/cokebar)
+# Version:     1.0.0 (2024.08.12)
+# Author:      Jackson Tong
+
+export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
+# Get the current script's path
+ROOT_PATH="$(dirname $(dirname "$(readlink -f "$0")"))"
+
+IPSET_NAME=LIBERTY_ADDRESS_GRP
+DNS_IP=8.8.8.8
+DNS_PORT=53
+OUT_FILE="${ROOT_PATH}/conf/dnsmasq.d/dnsmasq_gfw_github.conf"
+BASE64_DECODE='base64 -d'
+SED_ERES='sed -r'
+
+# Function to log messages
+log() {
+    local log_file="/var/log/gfw.log"
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] $1" | tee -a "$log_file"
+}
+
+clean_and_exit(){
+    # Clean up temp files
+    printf 'Cleaning up... '
+    #rm -rf $TMP_DIR
+    echo -e 'Done\n\n'
+    [ $1 -eq 0 ] && echo -e 'Job Finished.\n\n' || echo -e 'Exit with Error code '$1'.\n'
+    exit $1
+}
+
+main() {
+    # Set Global Var
+    BASE_URL='https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt'
+    #TMP_DIR=$(mktemp -d)
+    TMP_DIR=/tmp
+    BASE64_FILE="$TMP_DIR/base64.txt"
+    GFWLIST_FILE="$TMP_DIR/gfwlist.txt"
+    DOMAIN_TEMP_FILE="$TMP_DIR/gfwlist2domain.tmp"
+    DOMAIN_FILE="$TMP_DIR/gfwlist2domain.txt"
+    CONF_TMP_FILE="$TMP_DIR/gfwlist.conf.tmp"
+    OUT_TMP_FILE="$TMP_DIR/gfwlist.out.tmp"
+
+    # Fetch GfwList and decode it into plain text
+    # Determine the interface for routing to 8.8.8.8
+    INTERFACE=$(ip -4 --oneline route show 8.8.8.8 | awk '{print $3}')
+
+    echo "Downloading GfwList from github"
+    # Check if the interface was found
+    if [ ! -z "$INTERFACE" ]; then
+        # Download the file to the temporary file using the jailbreak route
+        CURL_EXTARG="--interface $INTERFACE"
+    fi
+
+    sudo curl -# -L $CURL_EXTARG -o $BASE64_FILE $BASE_URL
+
+    if [ $? != 0 ]; then
+        echo -e '\nFailed to fetch gfwlist.txt. Please check your Internet connection, and check TLS support for curl/wget.\n'
+        clean_and_exit 2
+    fi
+    $BASE64_DECODE $BASE64_FILE > $GFWLIST_FILE || ( echo -e 'Failed to decode gfwlist.txt. Quit.\n'; clean_and_exit 2 )
+    echo -e 'Done.\n\n'
+
+    # Convert
+    IGNORE_PATTERN='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+    HEAD_FILTER_PATTERN='s#^(\|\|?)?(https?://)?##g'
+    TAIL_FILTER_PATTERN='s#/.*$|%2F.*$##g'
+    DOMAIN_PATTERN='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
+    HANDLE_WILDCARD_PATTERN='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*[a-zA-Z0-9]*)?#\4#g'
+
+    printf 'Converting GfwList to ' && echo $OUT_TYPE && printf ' ...\n' 
+    #echo -e '\nWARNING:\nThe following lines in GfwList contain regex, and might be ignored:\n\n'
+    #cat $GFWLIST_FILE | grep -n '^/.*$'
+    #echo "\nThis script will try to convert some of the regex rules. But you should know this may not be a equivalent conversion.\nIf there's regex rules which this script do not deal with, you should add the domain manually to the list.\n\n"
+    grep -vE $IGNORE_PATTERN $GFWLIST_FILE | $SED_ERES $HEAD_FILTER_PATTERN | $SED_ERES $TAIL_FILTER_PATTERN | grep -E $DOMAIN_PATTERN | $SED_ERES $HANDLE_WILDCARD_PATTERN > $DOMAIN_TEMP_FILE
+
+    printf 'google.com\ngoogle.ad\ngoogle.ae\ngoogle.com.af\ngoogle.com.ag\ngoogle.com.ai\ngoogle.al\ngoogle.am\ngoogle.co.ao\ngoogle.com.ar\ngoogle.as\ngoogle.at\ngoogle.com.au\ngoogle.az\ngoogle.ba\ngoogle.com.bd\ngoogle.be\ngoogle.bf\ngoogle.bg\ngoogle.com.bh\ngoogle.bi\ngoogle.bj\ngoogle.com.bn\ngoogle.com.bo\ngoogle.com.br\ngoogle.bs\ngoogle.bt\ngoogle.co.bw\ngoogle.by\ngoogle.com.bz\ngoogle.ca\ngoogle.cd\ngoogle.cf\ngoogle.cg\ngoogle.ch\ngoogle.ci\ngoogle.co.ck\ngoogle.cl\ngoogle.cm\ngoogle.cn\ngoogle.com.co\ngoogle.co.cr\ngoogle.com.cu\ngoogle.cv\ngoogle.com.cy\ngoogle.cz\ngoogle.de\ngoogle.dj\ngoogle.dk\ngoogle.dm\ngoogle.com.do\ngoogle.dz\ngoogle.com.ec\ngoogle.ee\ngoogle.com.eg\ngoogle.es\ngoogle.com.et\ngoogle.fi\ngoogle.com.fj\ngoogle.fm\ngoogle.fr\ngoogle.ga\ngoogle.ge\ngoogle.gg\ngoogle.com.gh\ngoogle.com.gi\ngoogle.gl\ngoogle.gm\ngoogle.gp\ngoogle.gr\ngoogle.com.gt\ngoogle.gy\ngoogle.com.hk\ngoogle.hn\ngoogle.hr\ngoogle.ht\ngoogle.hu\ngoogle.co.id\ngoogle.ie\ngoogle.co.il\ngoogle.im\ngoogle.co.in\ngoogle.iq\ngoogle.is\ngoogle.it\ngoogle.je\ngoogle.com.jm\ngoogle.jo\ngoogle.co.jp\ngoogle.co.ke\ngoogle.com.kh\ngoogle.ki\ngoogle.kg\ngoogle.co.kr\ngoogle.com.kw\ngoogle.kz\ngoogle.la\ngoogle.com.lb\ngoogle.li\ngoogle.lk\ngoogle.co.ls\ngoogle.lt\ngoogle.lu\ngoogle.lv\ngoogle.com.ly\ngoogle.co.ma\ngoogle.md\ngoogle.me\ngoogle.mg\ngoogle.mk\ngoogle.ml\ngoogle.com.mm\ngoogle.mn\ngoogle.ms\ngoogle.com.mt\ngoogle.mu\ngoogle.mv\ngoogle.mw\ngoogle.com.mx\ngoogle.com.my\ngoogle.co.mz\ngoogle.com.na\ngoogle.com.nf\ngoogle.com.ng\ngoogle.com.ni\ngoogle.ne\ngoogle.nl\ngoogle.no\ngoogle.com.np\ngoogle.nr\ngoogle.nu\ngoogle.co.nz\ngoogle.com.om\ngoogle.com.pa\ngoogle.com.pe\ngoogle.com.pg\ngoogle.com.ph\ngoogle.com.pk\ngoogle.pl\ngoogle.pn\ngoogle.com.pr\ngoogle.ps\ngoogle.pt\ngoogle.com.py\ngoogle.com.qa\ngoogle.ro\ngoogle.ru\ngoogle.rw\ngoogle.com.sa\ngoogle.com.sb\ngoogle.sc\ngoogle.se\ngoogle.com.sg\ngoogle.sh\ngoogle.si\ngoogle.sk\ngoogle.com.sl\ngoogle.sn\ngoogle.so\ngoogle.sm\ngoogle.sr\ngoogle.st\ngoogle.com.sv\ngoogle.td\ngoogle.tg\ngoogle.co.th\ngoogle.com.tj\ngoogle.tk\ngoogle.tl\ngoogle.tm\ngoogle.tn\ngoogle.to\ngoogle.com.tr\ngoogle.tt\ngoogle.com.tw\ngoogle.co.tz\ngoogle.com.ua\ngoogle.co.ug\ngoogle.co.uk\ngoogle.com.uy\ngoogle.co.uz\ngoogle.com.vc\ngoogle.co.ve\ngoogle.vg\ngoogle.co.vi\ngoogle.com.vn\ngoogle.vu\ngoogle.ws\ngoogle.rs\ngoogle.co.za\ngoogle.co.zm\ngoogle.co.zw\ngoogle.cat\n' >> $DOMAIN_TEMP_FILE
+    printf 'Google search domains... ' && echo -e 'Added\n'
+
+    # Add blogspot domains
+    printf 'blogspot.ca\nblogspot.co.uk\nblogspot.com\nblogspot.com.ar\nblogspot.com.au\nblogspot.com.br\nblogspot.com.by\nblogspot.com.co\nblogspot.com.cy\nblogspot.com.ee\nblogspot.com.eg\nblogspot.com.es\nblogspot.com.mt\nblogspot.com.ng\nblogspot.com.tr\nblogspot.com.uy\nblogspot.de\nblogspot.gr\nblogspot.in\nblogspot.mx\nblogspot.ch\nblogspot.fr\nblogspot.ie\nblogspot.it\nblogspot.pt\nblogspot.ro\nblogspot.sg\nblogspot.be\nblogspot.no\nblogspot.se\nblogspot.jp\nblogspot.in\nblogspot.ae\nblogspot.al\nblogspot.am\nblogspot.ba\nblogspot.bg\nblogspot.ch\nblogspot.cl\nblogspot.cz\nblogspot.dk\nblogspot.fi\nblogspot.gr\nblogspot.hk\nblogspot.hr\nblogspot.hu\nblogspot.ie\nblogspot.is\nblogspot.kr\nblogspot.li\nblogspot.lt\nblogspot.lu\nblogspot.md\nblogspot.mk\nblogspot.my\nblogspot.nl\nblogspot.no\nblogspot.pe\nblogspot.qa\nblogspot.ro\nblogspot.ru\nblogspot.se\nblogspot.sg\nblogspot.si\nblogspot.sk\nblogspot.sn\nblogspot.tw\nblogspot.ug\nblogspot.cat\n' >> $DOMAIN_TEMP_FILE
+    printf 'Blogspot domains... ' && echo -e 'Added\n'
+
+    # Add twimg.edgesuite.net
+    printf 'twimg.edgesuite.net\n' >> $DOMAIN_TEMP_FILE
+    printf 'twimg.edgesuite.net... ' && echo -e 'Added\n'
+
+    echo -e 'Ipset rules included.'
+    sort -u $DOMAIN_TEMP_FILE | $SED_ERES 's#(.+)#server=/\1/'$DNS_IP'\#'$DNS_PORT'\
+ipset=/\1/'$IPSET_NAME'#g' > $CONF_TMP_FILE
+
+    # Generate output file
+    echo -e '# dnsmasq rules generated by gfwlist' > $OUT_TMP_FILE
+    echo "# Last Updated on $(date "+%Y-%m-%d %H:%M:%S")" >> $OUT_TMP_FILE
+    echo -e '# ' >> $OUT_TMP_FILE
+    cat $CONF_TMP_FILE >> $OUT_TMP_FILE
+    sudo cp $OUT_TMP_FILE $OUT_FILE
+    printf '\nConverting GfwList to '$OUT_TYPE'... ' && echo -e 'Done\n\n'
+
+    log "gfwlist updated"
+
+    # restart dnsmasq
+    pid=$(netstat -tulnp | grep 0.0.0.0:55353 | grep tcp | awk '{print $7}' | cut -d'/' -f1)
+
+    log "restart dnsmasq with pid $pid"
+
+    kill -HUP $pid
+
+    # Clean up
+    clean_and_exit 0
+}
+
+main "$@"
